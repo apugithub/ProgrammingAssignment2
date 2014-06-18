1efb040aa8f8fae14eba66017e93ae0b5593f30a
apugithub <apugithub@users.noreply.github.com>
2014-06-18 14:59:17 +0530

    Updated makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## A couple of functions that cache the inverse of a matrix
+## This function creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+
+## Matrix initialization
   i  <- NULL
+  ## Now setting the matrix
         set  <- function(y){
                 x <<- y
                 i <<- NULL 
         }
-         get  <- function() x
+## Getting the matrix and returning it        
+         get  <- function() { 
+         x
+         }
+## Method to set the inverse of the matrix       
         setinverse  <- function(inverse) {
         i  <<- inverse
         }
-        getinverse  <- function() i
+## Now getting the inverse of a matrix and returning it        
+        getinverse  <- function()  {
+        i
+        }
+## Returning a list of the methods        
         list(set= set, get = get, 
              setinverse = setinverse, 
              getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+
+
+
+
+## Compute the inverse of the special matrix returned by "makeCacheMatrix"  above
+## If the inverse has already been calculated (and the matrix has not changed)
+## then the "cachesolve" should give the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
          i  <- x$getinverse()
+## Check if the inverse matrix is already there         
         if (!is.null(i)){
                 message("getting cached data")
                 return(i)
         }
+## Get the matrix from our object        
         data  <- x$get()
-        i  <- solve(data, ...)
+ ## Calculate the inverse using matrix multiplication        
+        i  <- solve(data) %*% data
+ ## Set the inverse to the object        
         x$setinverse(i)
+## Return the martix        
         i
 }
